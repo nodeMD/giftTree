73bb82e1a04412d39d143d31b0abbb5147928891
Maciej Dynieski <53050193+nodeMD@users.noreply.github.com>
2025-12-20 09:13:37 +0100

    app: add icon

--- gift-tree/app/index.tsx
+++ gift-tree/app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { Image, View } from "react-native";
+import { images } from "@/constants/images";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1">
+      <Image
+        className="justify-center object-cover object-center rounded-full h-48 w-48"
+        source={images.icon}
+      />
     </View>
   );
 }
